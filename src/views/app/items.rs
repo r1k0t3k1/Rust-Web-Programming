85c37cebd05ce6ee7ceafc660dd407ff7a99b687
rikoteki <r1k0t3k1@bird.ocn.ne.jp>
2024-06-18 23:29:51 +0900

    add html view

---
+++ src/views/app/items.rs
@@ -0,0 +1,9 @@
+use actix_web::HttpResponse;
+use super::content_loader::read_file;
+
+pub async fn items() -> HttpResponse {
+    let html_data = read_file("./src/templates/main.html");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=UTF-8")
+        .body(html_data)
+}
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,11 +1,14 @@
 mod auth;
 mod to_do;
+mod app;
 
 use actix_web::web::ServiceConfig;
 use auth::auth_views_factory;
 use to_do::to_do_views_factory;
+use app::app_views_factory;
 
 pub fn views_factory(app: &mut ServiceConfig) {
     auth_views_factory(app);
     to_do_views_factory(app);
+    app_views_factory(app);
 }
